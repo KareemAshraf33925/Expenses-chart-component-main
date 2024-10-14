bdec5dd13548c824be1eea2ef9a638dd7a242444
Kareem ashraf <ashrafkareem005@gmail.com>
2024-10-14 06:30:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+// firstgrid
+var gridmon=document.querySelector(".grid_mon");
+var to=document.querySelector(".top");
+gridmon.addEventListener("click",()=>{
+    gridmon.classList.toggle("active");
+    to.classList.toggle("active")
+})
+// secondgrid
+var gridtue=document.querySelector(".grid_tue");
+var top_t=document.querySelector(".top_t");
+gridtue.addEventListener("click",()=>{
+    gridtue.classList.toggle("active");
+    top_t.classList.toggle("active")
+})
+// threegrid
+var gridwed=document.querySelector(".grid_wed");
+var top_w=document.querySelector(".top_w");
+gridwed.addEventListener("click",()=>{
+    gridwed.classList.toggle("active");
+    top_w.classList.toggle("active")
+})
+// fourgrid
+var gridthu=document.querySelector(".grid_thu");
+var top_th=document.querySelector(".top_th");
+gridthu.addEventListener("click",()=>{
+    gridthu.classList.toggle("active");
+    top_th.classList.toggle("active")
+})
+// fivrgrid
+var gridfri=document.querySelector(".grid_fri");
+var top_f=document.querySelector(".top_f");
+gridfri.addEventListener("click",()=>{
+    gridfri.classList.toggle("active");
+    top_f.classList.toggle("active")
+})
+// sixgrid
+var gridsat=document.querySelector(".grid_sat");
+var top_s=document.querySelector(".top_s");
+gridsat.addEventListener("click",()=>{
+    gridsat.classList.toggle("active");
+    top_s.classList.toggle("active")
+})
+// sevengrid
+var gridsun=document.querySelector(".grid_sun");
+var top_su=document.querySelector(".top_su")
+gridsun.addEventListener("click",()=>{
+    gridsun.classList.toggle("active");
+    top_su.classList.toggle("active")
+})
